074659ca6b0807b5ab2a7cb55c9208dcd495710c
freyafuglo <frey3246@stud.kea.dk>
2026-07-09 22:09:59 +0200

    adds api call

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import requests
+
+
+
+def get_coordinates(city):
+    api_url = "https://geocoding-api.open-meteo.com/v1/search"
+
+    response = requests.get(
+        api_url,
+        params={
+            "name": city,
+            "count": 1,
+        },
+    )
+
+    print(response.json())
+
 
 def weather(request):
-    return JsonResponse({
-        "city": "Copenhagen",
-        "temperature": 18,
-    })
+
+    city = "Copenhagen"
+    get_coordinates(city)
+
+
+    api_url = "https://jsonplaceholder.typicode.com/todos/1"
+    response = requests.get(api_url)
+    #print(response.json())
+
+    return JsonResponse(response.json())
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,34 @@
 <script setup>
 import { ref, onMounted } from "vue";
 
+
+const city = ref("");
 const weather = ref(null);
 
-onMounted(async () => {
+//const message = ref("");
+
+
+
+async function getWeather() {
   const response = await fetch("http://127.0.0.1:8000/api/weather/");
   weather.value = await response.json();
-});
+}
 </script>
 
 <template>
   <div>
     <h1>Weather App</h1>
 
+    <!--p>Message is: {{ message }}</p-->
+
+    <input v-model="city" placeholder="edit me" />
+
+    <button @click="getWeather">Submit</button>
+
     <div v-if="weather">
-      <p>City: {{ weather.city }}</p>
-      <p>Temperature: {{ weather.temperature }}°C</p>
+      <p>City: {{ city }}</p>
+      <!--p>Temperature: {{ weather.temperature }}°C</p-->
+      <p>Title: {{ weather.title }}</p>
     </div>
 
     <div v-else>
